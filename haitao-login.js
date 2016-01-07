68d414090767c076c512c321cd0e0cb030c0aa80
wanghsinche <wanghsinche@hotmail.com>
2016-01-07 19:47:48 +0800

    test

--- haitao-login.js
+++ haitao-login.js
@@ -10,9 +10,61 @@
 
 var btnposition="right";//选项为 left  right  和 center
 var accountlst=[
-	{username:'2323861569@qq.com',password:'ilovechina..',oname:'杜子腾'},
-	{username:'du0329@qq.com',password:'ilovechina..',oname:'杜琪琪'}
+	{username:'292677146@qq.com',password:'huihuihaitao',oname:'杜洪磊'},
+    {username:'15069012326@qq.com',password:'ilovechina..',oname:'杜洪磊磊'},
+    {username:'hongleidu@vip.qq.com',password:'ilovechina..',oname:'杜朝月'},
+    {username:'lcez_tuzi@qq.com',password:'ilovechina..',oname:'杜洪'},
+    {username:'2323861569@qq.com',password:'ilovechina..',oname:'杜子腾'},
+    {username:'du0329@qq.com',password:'ilovechina..',oname:'杜琪琪'},
+    {username:'2271177214@qq.com',password:'ilovechina..',oname:'卢香香'},
 ];
+
+var FetchList=function(start,end){
+	this.loginTime=0;
+	this.start=start;
+	this.end=end;
+	this.accountindicator=0;
+};
+
+FetchList.prototype.fetch=function(start,end,callback){
+	var baseURL="http://buyers.youdao.com/order/myorders?";
+	callback.call(this);
+};
+
+FetchList.prototype.login=function(username,password){
+	if(loginTime>3){
+		return;
+	}
+	var loginURL='https://buyers.youdao.com/auth/login';
+	var text=["username="+encodeURIComponent(username),"password="+encodeURIComponent(password)];
+	var senddata=text.join('&');
+	this.loginTime++;
+	this.accountindicator++;
+	var _this=this;
+	var temp={username:username,password:password};
+	GM_xmlhttpRequest({
+		method:"POST",
+		url:'loginURL',
+		headers: {
+		    "User-Agent": "Mozilla/5.0", // If not specified, navigator.userAgent will be used.
+			'Content-Type': 'application/x-www-form-urlencoded'
+		},
+		data:senddata,
+		onload:function(response){
+			if (response.finalUrl==="http://buyers.youdao.com/list") {
+				_this.fetch(_this.start,_this.end,_this.login(accountlst[_this.accountindicator].username,accountlst[_this.accountindicator].password));
+			}else{
+				_this.login(temp.username,temp.password);
+			}
+		},
+		onerror:function(response){
+				_this.login(temp.username,temp.password);
+		}
+	});	
+};
+
+var myfetch=new FetchList(0,1);
+myfetch.loginlogin(accountlst[this.accountindicator].username,accountlst[this.accountindicator].password);
 
 var AccountClass=function(username,password,user){
 	var text=["username="+encodeURIComponent(username),"password="+encodeURIComponent(password)];
@@ -93,6 +145,8 @@ $('.breadcrumbs a').css('line-height:58px');
 for (var i = accountlst.length - 1; i >= 0; i--) {
 	(new AccountClass(accountlst[i]['username'],accountlst[i]['password'],accountlst[i]['oname'])).setUI();
 }
+
+//-----------------------------------------------------------------------------------------------------------
 
 
 
