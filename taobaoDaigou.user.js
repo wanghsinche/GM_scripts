5eb5e94326e807dfbc235f935dd4f8a9b06aa047
wanghsinche <wanghsinche@hotmail.com>
2016-01-30 21:37:37 +0800

    Update taobaoDaigou.user.js

--- taobaoDaigou.user.js
+++ taobaoDaigou.user.js
@@ -5,8 +5,7 @@
 // @author       Wanghsinche 
 // @include      http://www.amazon.co.uk/*
 // @include      http://daigou.taobao.com
-
-
+// @require http://libs.useso.com/js/jquery/1.9.0/jquery.min.js
 // @grant       GM_xmlhttpRequest
 // @grant       GM_setValue
 // @grant       GM_getValue
@@ -32,6 +31,44 @@ Event.prototype = {
     }
 };
 //////////////////////////////////
+var afterLoaded=function(response){
+	var existParttern="商品已经存在";
+	var successParttern="itemPublish.htm";
+	var hasPostedParttern="已经存在该商品";
+	if (response.finalUrl.match(existParttern)===null) {
+		//商品不存在，继续
+		if (response.finalUrl.match(successParttern)===null){
+			//未知类型，退出
+			// alert('未知类型');
+			console.log('1');
+		}else{
+			//已知类型
+			if (response.responseText.match(hasPostedParttern)===null) {
+				//可以提交
+				GM_openInTab(response.finalUrl, true);
+			}else{
+				//已经提交
+				// alert(hasPostedParttern);
+							console.log('2');
+
+			}
+			
+		}
+	}
+	else{
+		//商品存在
+		// alert(existParttern);
+					console.log('3');
+
+	}
+
+};
+//////////////////////////////////
+var parseHtml=function(response){
+	var $html=$(response.responseText);
+
+};
+//////////////////////////////////
 var fetchLogin=function(i){
 	var loginEvent= new Event(this);	
 	var loginURL='http://daigou.taobao.com/buyer/index.htm';
@@ -39,7 +76,7 @@ var fetchLogin=function(i){
 	var senddata=text.join('&');
 	var _this=this;
 	GM_xmlhttpRequest({
-		method:"GET",
+		method:"POST",
 		url:loginURL,
 		headers: {
 		    "User-Agent": "Mozilla/5.0", // If not specified, navigator.userAgent will be used.
@@ -47,20 +84,19 @@ var fetchLogin=function(i){
 		},
 		data:senddata,
 		onload:function(response){
-			// if (response.finalUrl==="http://buyers.youdao.com/list") {
-			// 	globalLoginDoneEvent.notify(tempuser);
-			// }else{
-			// 	globalFetchDoneEvent.notify();//skip and next account
-			// 	console.log("username :" + tempuser+'login failed');
-			// 	return false;
-			// }
-			console.log(response);
-			alert(response.finalUrl);
+			afterLoaded(response);
 		},
 		onerror:function(response){
 				// globalFetchDoneEvent.notify();//skip and next account
 				// console.log("username :" + tempuser+'login failed');
 				// return false;
+				alert('未知错误');
 		}
 	});	
 };
+
+////////////////////
+$('#productTitle').append('<button id="wxzBtn" class="">check</button>');
+$('#wxzBtn').click(function(){
+	fetchLogin();
+});
